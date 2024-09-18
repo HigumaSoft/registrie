cd3523fd85b798468939e1659967a1f421fce4a0
Dmitry Mikhaylov <business.mikhaylov@gmail.com>
2024-09-19 00:57:16 +0900

    init commit

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
+  {files: ["src/**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
